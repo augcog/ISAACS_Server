587dc951714eb0bc69a494565045db739f688c69
Varun Saran <varunsaran@berkeley.edu>
2020-10-12 19:47:53 -0700

    added control_drone_service to simulate advertising the drone's services

---
+++ src/simulate_drone_services/control_drone_service.py
@@ -0,0 +1,23 @@
+import roslibpy
+
+# Since we don't have the physical drone to advertise its services (land, takeoff, etc)
+# this script simulates advertising those services. This lets the server actually call them.
+# Instead of seeing the drone land, at least we can see a print message saying the drone would be landing if we had one
+
+
+client = roslibpy.Ros(host='136.25.185.6', port=9090)
+client.run()
+
+print(client.is_connected)
+
+#TODO: DroneTaskControl should take care of land, takeoff, etc, depending on the input to request["task"] (4 = takeoff, 6 = land, 1 = go home)
+#We might not need to actually implement each case, depending on how precise we want this "simulator" to be
+def handler(request, response):
+    print("Drone land service is being 'simulated'")
+    response["success"] = true
+
+
+service = roslibpy.Service(client, '/land_drone_0', 'dji_sdk/DroneTaskControl')
+service.advertise(handler)
+
+client.run_forever()
