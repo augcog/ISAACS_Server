a0bb4b9435c1347790516f07aed836a1b52c7cb8
DragonKyro <kyle31412@gmail.com>
2020-10-26 18:12:53 -0700

    delete unecessary class files

--- src/topic_types.py
+++
@@ -1,8 +0,0 @@
-class topic_types():
-    def __init__(self, JSON):
-        '''
-        name = string with name of topic
-        type = string with type of topic
-        '''
-        self.name = JSON.name
-        self.type = JSON.type
