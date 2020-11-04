fb2c16ba7d426396362e3058cc657cee3bb308b4
Leonardo Tang <leonardodtang@berkeley.edu>
2020-11-03 18:11:01 -0800

    moved enums into Drone class, added structure for updating missions, need to implement helper for it to work

--- src/djimatrice_drone.py
+++ src/djimatrice_drone.py
@@ -1,13 +1,19 @@
 import roslibpy
 from drone import Drone
-
-class WaypointActions(Enum):
-    START = 0
-    STOP = 1
-    PAUSE = 2
-    RESUME = 3
+from enum import Enum
 
 class DjiMatriceDrone(Drone):
+
+    class UpdateMissionAction(Enum):
+        CONTINUE_MISSION = 0,
+        UPDATE_CURRENT_MISSION = 1,
+        END_AND_HOVER = 2
+
+    class WaypointActions(Enum):
+        START = 0
+        STOP = 1
+        PAUSE = 2
+        RESUME = 3
 
     drone_type = "DjiMatrice"
     ros_drone_connection = None
@@ -15,6 +21,7 @@ class DjiMatriceDrone(Drone):
     def __init__(self, drone_name, drone_type, id=False):
         super().__init__(drone_name, drone_type, id)
         assert(drone_type == self.drone_type)
+        self.prev_flight_status = Drone.Flight_Status.NULL
 
     # TODO Implement
     def upload_mission(self, waypoints):
@@ -26,10 +33,10 @@ class DjiMatriceDrone(Drone):
             for i in range(len(self.waypoints)):
                 # TODO: Get waypoint message from waypoint
                 way_point_msg = waypoints[i]
-                mission_msg_list.append(way_point_msg)
+                self.mission_msg_list.append(way_point_msg)
             # TODO: Create task msg, discuss how to do this
             way_point_task = way_point_msg
-            upload_waypoint_task(way_point_task)
+            self.upload_waypoint_task(way_point_task)
 
         return False
 
@@ -56,46 +63,59 @@ class DjiMatriceDrone(Drone):
             result = service.call(request)
             print('Service response: {}'.format(result))
         except:
-            result = {"success":False, "message":"Drone landing failed"}
+            result = {"success":False, "message":"Failed to set new drone speed"}
+        return result
+
+    def fetch_speed(self):
+        try:
+            print("Attempting to fetch speed...")
+            service = roslibpy.Service(self.ROS_master_connection, 'dji_sdk/mission_waypoint_getSpeed', 'dji_sdk/MissionWpGetSpeed')
+            request = roslibpy.ServiceRequest()
+
+            print('Calling mission_waypoint_setSpeed service...')
+            result = service.call(request)
+            print('Service response: {}'.format(result))
+        except:
+            result = {"success":False, "message":"Failed to fetch drone speed"}
         return result
 
     def start_mission(self):
         try:
             print("Attempting to start drone mission...")
             service = roslibpy.Service(self.ROS_master_connection, 'dji_sdk/mission_waypoint_action', 'dji_sdk/MissionWpAction')
-            request = roslibpy.ServiceRequest({"action": WaypointActions.START})
+            request = roslibpy.ServiceRequest({"action": Drone.WaypointActions.START})
 
             print('Calling mission_waypoint_action start service...')
             result = service.call(request)
             print('Service response: {}'.format(result))
         except:
-            result = {"success":False, "message":"Drone landing failed"}
+            result = {"success":False, "message":"Mission failed to start"}
         return result
 
     def pause_mission(self):
         try:
             print("Attempting to pause drone mission...")
             service = roslibpy.Service(self.ROS_master_connection, 'dji_sdk/mission_waypoint_action', 'dji_sdk/MissionWpAction')
-            request = roslibpy.ServiceRequest({"action": WaypointActions.PAUSE})
+            request = roslibpy.ServiceRequest({"action": Drone.WaypointActions.PAUSE})
 
             print('Calling mission_waypoint_action pause service...')
             result = service.call(request)
             print('Service response: {}'.format(result))
         except:
-            result = {"success":False, "message":"Drone landing failed"}
+            result = {"success":False, "message":"Mission failed to pause"}
         return result
 
     def resume_mission(self):
         try:
             print("Attempting to resume drone mission...")
             service = roslibpy.Service(self.ROS_master_connection, 'dji_sdk/mission_waypoint_action', 'dji_sdk/MissionWpAction')
-            request = roslibpy.ServiceRequest({"action": WaypointActions.RESUME})
+            request = roslibpy.ServiceRequest({"action": Drone.WaypointActions.RESUME})
 
             print('Calling mission_waypoint_action resume service...')
             result = service.call(request)
             print('Service response: {}'.format(result))
         except:
-            result = {"success":False, "message":"Drone landing failed"}
+            result = {"success":False, "message":"Mission failed to resume"}
         return result
 
     # TODO Implement
@@ -130,4 +150,30 @@ class DjiMatriceDrone(Drone):
             print('Service response: {}'.format(result))
         except:
             result = {"success":False, "message":"Drone flying home failed"}
+        return result
+
+    #TODO Implement
+    def update_mission_helper(self, action):
+        if action == Drone.UpdateMissionAction.CONTINUE_MISSION:
+            # Call corresponding service and return result
+            result = {"success":True, "message":"Vacuously true for testing"}
+        elif action == Drone.UpdateMissionAction.UPDATE_CURRENT_MISSION:
+            # Call corresponding service and return result
+            result = {"success":True, "message":"Vacuously true for testing"}
+        elif action == Drone.UpdateMissionAction.END_AND_HOVER:
+            # Call corresponding service and return result
+            result = {"success":True, "message":"Vacuously true for testing"}
+        return result
+    
+    # TODO: Need to implement update_mission_helper to work
+    def update_mission(self):
+        if self.flight_status == Drone.Flight_Status.FLYING_HOME:
+            result = self.update_mission_helper(Drone.UpdateMissionAction.UPDATE_CURRENT_MISSION)
+        elif self.flight_status == Drone.Flight_Status.ON_GROUND_STANDBY:
+            if self.prev_flight_status != Drone.Flight_Status.NULL:
+                result = self.update_mission_helper(Drone.UpdateMissionAction.CONTINUE_MISSION)
+        elif self.flight_status == Drone.Flight_Status.IN_AIR_STANDBY:
+            result = self.update_mission_helper(Drone.UpdateMissionAction.CONTINUE_MISSION)
+        else:
+            result = {"success":False, "message":"Invalid Request: Could not update mission"}
         return result
--- src/drone.py
+++ src/drone.py
@@ -2,16 +2,27 @@ import roslibpy
 from abc import ABC, abstractmethod
 from enum import Enum
 
-class Flight_Status(Enum):
-    ON_GROUND_STANDBY = 1
-    IN_AIR_STANDBY = 2
-    FLYING = 3
-    FLYING_HOME = 4
-    PAUSED_IN_AIR = 5
-    LANDING = 6
-    NULL = 7
-
 class Drone(ABC):
+
+    class Flight_Status(Enum):
+        ON_GROUND_STANDBY = 1
+        IN_AIR_STANDBY = 2
+        FLYING = 3
+        FLYING_HOME = 4
+        PAUSED_IN_AIR = 5
+        LANDING = 6
+        NULL = 7
+        
+    class UpdateMissionAction(Enum):
+        CONTINUE_MISSION = 0,
+        UPDATE_CURRENT_MISSION = 1,
+        END_AND_HOVER = 2
+
+    class WaypointActions(Enum):
+        START = 0
+        STOP = 1
+        PAUSE = 2
+        RESUME = 3
 
     speed = 5
     ROS_master_connection = roslibpy.Ros(host='136.25.185.6', port=9090)
@@ -21,7 +32,7 @@ class Drone(ABC):
         self.drone_type = drone_type
         self.drone_name= drone_name
         self.connection_status = False
-        self.flight_status = Flight_Status.NULL
+        self.flight_status = Drone.Flight_Status.NULL
         self.topics = []
         self.sensors = []
 
