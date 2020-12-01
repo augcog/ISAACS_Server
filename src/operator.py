b40be4df18fb5a24abb0cf2a2c88b5c0b80e342d
DragonKyro <kyle31412@gmail.com>
2020-11-30 20:39:59 -0500

    operator print statements

--- src/operator.py
+++ src/operator.py
@@ -50,33 +50,25 @@ def custom_service(handler):
 ################################
 # Interface -> Server Handlers #
 ################################
-
-# Todo Implement
 @custom_service
 def all_drones_available(request, response):
-
-    #dont have list of services
-    #drones_available = {k: {drone_name: v.name, drone_subs: v.topics} for k, v in drones}
-
     drones_available = []
-    print("Calling all_drones_available_service ...")
+    print("Calling all_drones_available service...")
     for k, v in drones.items():
         avail = dict()
         avail["id"] = k
         avail["name"] = v.drone_name
         avail["type"] = v.drone_type
         avail["topics"] = v.topics
-        #TODO fix services
         avail["services"] = v.services
         drones_available.append(avail)
 
     response["success"] = True
     response["message"] = "Successfully sent all available drones to VR"
-    #response['drones_available'] = []
     response['drones_available'] = drones_available
-    print("All_drones_available_service finished")
-
+    print("All_drones_available service finished!")
     return True
+
 
 @custom_service
 def upload_mission(request, response):
@@ -84,44 +76,64 @@ def upload_mission(request, response):
     :param request: dict of {drone_id: int, waypoints: list of ints/strings --> pass
     these directly into the drone instance}
     '''
+    print("Calling upload_mission service...")
     if not drones[request["drone_id"]]:
         response["success"] = False
         response["message"] = "No drone with that id."
         response["drone_id"] = -1
         return False
     response = drones[request["drone_id"]].upload_mission(request["waypoints"])
+    print("Upload_mission service finished!")
     return True
 
-# Todo Implement
+
 @custom_service
 def set_speed(request, response):
-    print('Setting speed to {}'.format(request['data']))
+    print("Calling set_speed service...")
+    if not drones[request["drone_id"]]:
+        response["success"] = False
+        response["message"] = "No drone with that id."
+        response["drone_id"] = -1
+        return False
+    print('Setting speed to {}...'.format(request['data']))
+    response = drones[request["drone_id"]].set_speed(request["data"])
     response['success'] = True
+    print("Set_speed service finished!")
     return True
 
-# Todo Implement
+
 # includes startmission, pausemission, resume mission, landdrone, flyhome
 @custom_service
 def control_drone(request, response):
+    print("Calling control_drone service...")
     control_task = request["control_task"]
     drone = drones.get(request["id"])
     if control_task == "start_mission":
+        print("Executing start_mission...")
         response = drone.start_mission()
     elif control_task == "pause_mission":
+        print("Executing pause_mission...")
         response = drone.pause_mission()
     elif control_task == "resume_mission":
+        print("Executing resume_mission...")
         response = drone.resume_mission()
     elif control_task == "land_drone":
+        print("Executing land_drone...")
         response = drone.land_drone()
     elif control_task == "fly_home":
+        print("Executing fly_home...")
         response = drone.fly_home()
     else:
         response["success"] = False
         response["message"] = "Invalid control task"
+        return False
+    print("Control_drone service finished!")
     return True
+
 
 @custom_service
 def query_topics(request, response):
+    print("Calling query_topics service...")
     id = request["id"]
     response["id"] = id
     all_topics_response = []
@@ -143,32 +155,31 @@ def query_topics(request, response):
     response["all_topics"] = all_topics_response
     response["success"] = True
     response["message"] = "Successfully queried topics."
+    print(all_topics_response)
+    print("Query_topics service finished!")
     return True
 
 ############################
 # Drone -> Server Handlers #
 ############################
-
 @custom_service
 def register_drone(request, response):
     '''
     :param request: dict of {drone_name: string, drone_type: string}
     '''
-
+    print("Calling register_drone service...")
     def get_id():
         global next_id
         cur_id, next_id = next_id, next_id + 1
         return cur_id
-
     drone_name = request["drone_name"]
     drone_type = request["drone_type"]
-    print(f"\tDroneType: {request['drone_type']}\n")
-
     # Create new drone instance using base class constructor, which should then
     # call child constructor corresponding to the drone_type
     d=Drone.create(drone_name, drone_type)
     successful=False
 
+    print(f"Adding drone {id} to global drones map...")
     if d:
         id = get_id()
         d.id=id
@@ -177,36 +188,32 @@ def register_drone(request, response):
         successful = True
         response["success"] = successful
         response["drone_id"] = id
-    print(f"Adding drone {id} to global drones map with following properties:")
-
-    #TODO fix message to error
     if successful:
         response["message"] = "Drone registered"
     else:
         response["message"] = "Failed to register drone"
-
     print(drones)
     print(drone_names)
+    print("Register_drone service finished!")
+    return True
 
-    return True # TODO check where this return goes to
 
 @custom_service
 def save_drone_topics(request, response):
+    print("Calling save_drone_topics...")
     publishes = request["publishes"]
     id = request["id"]
-
     if drones[id] == None:
         response["success"] = False
         response["message"] = "Drone id does not exist"
         return False
-
     for topic in publishes:
         all_topics[topic["name"]] = topic["type"]
         drones[id].topics.append(topic)
-
     response["success"] = True
     response["message"] = "Successfully saved drone topics"
     print(all_topics)
+    print("Save_drone_topics service finished!")
     return True
 
 
@@ -215,20 +222,19 @@ def shutdown_drone(request, response):
     '''
     :param request: message that has a drone_id: std_msgs/Int32 and drone_subs: issacs_server/topic[]
     '''
+    print("Calling shutdown_drone service...")
     id = request["id"]
     publishes = request["publishes"]
     successful = False
     if id in drones:
-
         drone_names.pop(drones[id].drone_name)
         drones.pop(id)
         for topic in publishes:
             all_topics.pop(topic['name'])
-
         # TODO ensure that drone instance is completely terminated
         # TODO Remove drone_subs from global topics dict
-
         successful = True
+
     response["success"] = successful
     if successful:
         response["message"] = "Drone successfully shutdown"
@@ -237,7 +243,7 @@ def shutdown_drone(request, response):
 
     print(drone_names)
     print(drones)
-    print(all_topics)
+    print("Shutdown_drone service finished!")
     return True
 
 
@@ -249,21 +255,18 @@ def register_sensor(request, response):
     '''
     :param request: dict of {drone_name: string, drone_type: string}
     '''
-
-    #TODO error checking fixes ID when bad Sensor init
+    print("Calling register_sensor service...")
     def get_id():
-        global next_id  #TODO FIXME
+        global next_id
         cur_id, next_id = next_id, next_id + 1
         return cur_id
-
     sensor_name = request["sensor_name"]
     sensor_type = request["sensor_type"]
     parent_drone_name = request["parent_drone_name"]
-    print(f"\tSensorType: {request['sensor_type']}\n")
-
     s=Sensor.create(drone_name, drone_type, drone_names[parent_drone_name])
     successful=False
 
+    print(f"Adding sensor {id} to global sensor map.")
     if s:
         id = get_id()
         s.id=id
@@ -273,46 +276,45 @@ def register_sensor(request, response):
         response["success"] = successful
         response["sensor_id"] = id
 
-    # TODO Instantiate Sensor Object
-    print(f"Adding sensor {id} to global sensor map with following properties:")
-
-    #TODO fix message to error
     if successful:
         response["message"] = "Registering sensor"
     else:
         response["message"] = "Failed to register sensor"
+    print(sensor_names)
+    print(sensors)
+    print("Register_sensor service finished!")
+    return True
 
-    return True # TODO check where this return goes to
 
 @custom_service
 def save_sensor_topics(request, response):
+    print("Calling save_sensor_topics service...")
     publishes = request["publishes"]
     id = request["id"]
-
     if sensors[id] == None:
         response["success"] = False
         response["message"] = "Sensor id does not exist"
         return False
-
     for topic in publishes:
         all_topics[topic["name"]] = topic["type"]
         sensors[id].topics.append(topic)
-
     response["success"] = True
     response["message"] = "Successfully saved sensor topics"
-
+    print(all_topics)
+    print("Save_sensor_topics service finished!")
     return True
+
 
 @custom_service
 def shutdown_sensor(request, response):
     '''
     :param request: message that has a sensor_id: std_msgs/Int32 and sensor_subs: issacs_server/topic[]
     '''
+    print("Calling shutdown_sensor service...")
     sensor_id = request["id"]
     publishes = request["publishes"]
     successful = False
     if sensor_id in sensors:
-        # TODO Fix when sensor class is done
         sensor_names.pop(sensors[id].sensor_name)
         sensors.pop(sensor_id)
         for topic in publishes:
@@ -325,9 +327,10 @@ def shutdown_sensor(request, response):
         response["message"] = "Sensor successfully shutdown"
     else:
         response["message"] = "Failed to shutdown sensor"
-
+    print(sensor_names)
+    print(sensors)
+    print("Shutdown_sensor service finished!")
     return True
-
 
 
 print('Services advertised.')
