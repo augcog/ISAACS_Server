4e060f2fd68a9abbac746d6f46608794fa3632e1
DragonKyro <kyle31412@gmail.com>
2021-02-20 11:32:19 -0800

    waypoint vs task

--- src/simulate_drone_services/control_drone_service.py
+++ src/simulate_drone_services/control_drone_service.py
@@ -13,17 +13,27 @@ Simulates advertising the service drone_control from the drone.
 TODO: DroneTaskControl should take care of land, takeoff, etc, depending on the input to request["task"] (4 = takeoff, 6 = land, 1 = go home)
 We might not need to actually implement each case, depending on how precise we want this "simulator" to be
 '''
-def handler(request, response):
+def drone_control(request, response):
     print("Control task service is being simulated")
     print(f"Performing task: {request['task']}")
     response["success"] = True
     response["message"] = f"Control task {request['task']} completed"
     return True
 
-service = roslibpy.Service(client, '/fake_drone_control', 'isaacs_server/fake_drone_control')
-service.advertise(handler)
+def drone_waypoint(request, response):
+    print("Drone waypoint service is being simulated")
+    print(f"Performing action: {request['action']}")
+    response["success"] = True
+    response["message"] = f"Waypoint action: {request['action']} completed"
+    return True
 
-print("Control tasks service advertised...")
+service = roslibpy.Service(client, 'isaacs_server/fake_drone_control', 'isaacs_server/fake_drone_control')
+service.advertise(drone_control)
+
+service2 = roslibpy.Service(client, 'isaacs_server/fake_drone_waypoint', 'isaacs_server/fake_drone_waypoint')
+service2.advertise(drone_waypoint)
+
+print("Fake drone services advertised...")
 
 client.run_forever()
 client.terminate()
