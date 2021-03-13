dbc4c08f5eea8b3b1865c16a4aeae2d341b36300
leonardodtang <leonardodtang@github.com>
2021-03-13 10:43:32 -0800

    updating upload_mission to handle goal parameter in operator.py

--- src/operator.py
+++ src/operator.py
@@ -623,7 +623,7 @@ def upload_mission(goal):
     if not d:
         server2.set_succeeded({"id":drone.id, "success":False, "message":"No drone with that id."})
     else:
-        callback = d.upload_mission(request["waypoints"])
+        callback = d.upload_mission(goal["waypoints"])
         print("Upload_mission action finished!")
         server2.send_feedback({"progress": "Upload_mission action finished!"})
         server2.set_succeeded({"id":drone.id, "success":callback["success"], "message":callback["message"]})
