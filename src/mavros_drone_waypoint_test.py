96e69e7fc0e6e08ecb22b1ab0d8b56f19cda5ff8
DragonKyro <kyle31412@gmail.com>
2021-04-10 12:38:21 -0700

    dji sim consolidation

--- src/mavros_drone_waypoint_test.py
+++
@@ -1,6 +0,0 @@
-from drone import Drone
-
-mavros_drone = Drone.create('mavros_drone', 'MavrosDrone')
-mavros_drone.upload_mission([
-    {'frame': 3, 'command': 16, 'is_current': False, 'autocontinue': True, 'param1': 0, 'param2': 0, 'param3': 0, 'x_lat': -35.364652, 'y_long': 149.163501, 'z_alt': 20},
-    {'frame': 3, 'command': 16, 'is_current': False, 'autocontinue': True, 'param1': 0, 'param2': 0, 'param3': 0, 'x_lat': -35.365361, 'y_long': 149.163995, 'z_alt': 20}])
